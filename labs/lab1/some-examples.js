45e2ab13e9953d81990b39f58c7f1bea105a0f66
Bobb-93 <borisiakimov@gmail.com>
2024-11-14 10:59:07 +0200

    Some examples

---
+++ labs/lab1/some-examples.js
@@ -0,0 +1,20 @@
+// това е коментар, който не се изпълнява от системата
+
+// показва „изскачащо“ съобщение с текст „Welcome“ - грешка при Node.js
+// alert("Welcome");
+console.log("Welcome");
+
+// декларира променливата answer
+let answer;
+
+// записва числото 42 в променливата answer
+answer = 42;
+
+// изписва стойността на answer в конзолата
+console.log( answer );
+
+// задава червен фон на документа - document is not defined в Node.js
+//document.body.style.backgroundColor = "red";
+
+let x234AB$$53;
+console.log(x234AB$$53);
