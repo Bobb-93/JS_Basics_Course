ef85d6199f756f1f61852f09168661ddc6a240e8
Bobb-93 <borisiakimov@gmail.com>
2024-11-30 19:53:06 +0200

    ADDED: lab5 files and functions.js from lab6

---
+++ labs/lab5/tmp.js
@@ -0,0 +1,18 @@
+let difficulty = 'eaSY';
+console.log( difficulty.toLowerCase());
+
+let stringNumbers = '1,2,3';
+let numbers = stringNumbers.split(',');
+console.log('---');
+console.log(numbers);
+
+let stringNumners2 = numbers.join(',');
+console.log('+++');
+
+console.log(stringNumners2);
+
+
+let arr = [1,2,3];
+arr.reverse();
+
+console.log( arr );
