073da60915d50c9fdd3a41e43787aa18f5f9effc
Bobb-93 <borisiakimov@gmail.com>
2024-12-03 11:28:43 +0200

    Example: 3.callerWithFunctionDeclarationOrExecution.js

---
+++ labs/lab7/3.callerWithFunctionDeclarationOrExecution.js
@@ -0,0 +1,15 @@
+function caller(f){
+    console.log("1")
+    f();
+}
+
+function callback(){
+    console.log("2")
+}
+
+console.log("Feeding the caller() with function declaration")
+caller( callback );
+
+// console.log("Feeding the caller() with function execution")
+// caller( callback() );
+//TypeError: f is not a function
