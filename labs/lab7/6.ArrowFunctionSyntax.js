253a5fc4605d2f764ffe87be917dd5b9fc012930
Bobb-93 <borisiakimov@gmail.com>
2024-12-04 10:13:58 +0200

    6.ArrowFunctionSyntax.js

---
+++ labs/lab7/6.ArrowFunctionSyntax.js
@@ -0,0 +1,13 @@
+const pi = 3.14;
+
+// let circleArea = (r)=>{return r*r*pi};
+
+// let circleArea = r=>{return r*r*pi};
+
+let circleArea = r=>r*r*pi;
+
+let circleAreaUndefined = r=>{r*r*pi};
+
+console.log(`circleArea(2) = ${circleArea(2)}`);
+
+console.log(`circleAreaUndefined(2) = ${circleAreaUndefined(2)}`);
