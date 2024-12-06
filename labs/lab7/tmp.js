f56dc25b063e25c0c7f99de321a5bc492894dc75
Bobb-93 <borisiakimov@gmail.com>
2024-12-06 18:26:03 +0200

    ADDED: tmp.js

---
+++ labs/lab7/tmp.js
@@ -0,0 +1,12 @@
+function querySelector() {
+    return null
+}
+
+let selectedOption = querySelector();
+
+console.log(selectedOption);
+
+
+if(selectedOption && selectedOption.value===5){
+    console.log(`OK`);
+}
