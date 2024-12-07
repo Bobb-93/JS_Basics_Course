1389117aa213c3034e087df7e3e61ff680a1fe18
Bobb-93 <borisiakimov@gmail.com>
2024-12-07 19:52:36 +0200

    ADDED: lab8 -> 4.inneffectiveObjects.js

---
+++ labs/lab8/4.inneffectiveObjects.js
@@ -0,0 +1,25 @@
+var developer1 = {
+    "firstName" : "Ivan",
+    "surName"   : "Ivanov",
+    "skills"     : ["HTML", "CSS", "JavaScript"],
+
+    "applyForJob": function(){
+        var skillsStr = this.skills.toString();
+
+        console.log(`Hi, I'm ${this.firstName} ${this.surName}. My strongest skills are ${skillsStr}.`);
+    }
+}
+var developer2 = {
+    "firstName" : "Maria",
+    "surName"   : "Mineva",
+    "skills"     : ["Python", "Machine Learning", "Data Science"],
+
+    "applyForJob": function(){
+        var skillsStr = this.skills.toString();
+
+        console.log(`Hi, I'm ${this.firstName} ${this.surName}. My strongest skills are ${skillsStr}.`);
+    }
+}
+
+developer1.applyForJob();
+developer2.applyForJob();
