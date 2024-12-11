de67b843a5c2cbded4040cb9f669f32ba379f2fb
Bobb-93 <borisiakimov@gmail.com>
2024-12-11 14:37:06 +0200

    ADDED: lab -> 2.Exercise.html, 1.SimpleExample.js

---
+++ labs/lab9/1.simpleExamples.js
@@ -0,0 +1,6 @@
+// using built in Math object
+console.log( Math.max(2,12,5)); // 12
+
+// using built in Date() constructor
+let currentDate = new Date();
+console.log( currentDate.getFullYear() ); // 2024
