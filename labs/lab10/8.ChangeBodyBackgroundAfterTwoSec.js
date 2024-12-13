3fcfaee3fcd28a7d9d7d25b6562dbc09ca36989e
Bobb-93 <borisiakimov@gmail.com>
2024-12-13 19:27:32 +0200

    ADDED: 8.ChangeBodyBackgroundAfterTwoSec.html and js

---
+++ labs/lab10/8.ChangeBodyBackgroundAfterTwoSec.js
@@ -0,0 +1,10 @@
+var color = "#DAA371";
+
+function changeBodyBG(){
+    console.log(`changeBodyBG() will change the body background!`);
+    document.body.style.backgroundColor = color;
+}
+
+setTimeout(changeBodyBG, 2000);
+
+console.log(`After setTimeout()`);
