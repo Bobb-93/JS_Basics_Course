17032965675f5b1cab04b53e1c96b1fa7dcfc7b2
Bobb-93 <borisiakimov@gmail.com>
2024-12-14 16:48:42 +0200

    ADDED: lab10 -> 13.ThrowErrorValidateAge.js

---
+++ labs/lab10/13.ThrowErrorValidateAge.js
@@ -0,0 +1,12 @@
+function validateAge(age) {
+    if(age<=0 || age>120){
+        // throw an Error:
+        throw new Error('Invalid value for age!')
+    }else{
+        console.log(`Age is valid: ${age}`);
+    }
+}
+
+validateAge(34);
+// validateAge(-5);
+validateAge(121);
