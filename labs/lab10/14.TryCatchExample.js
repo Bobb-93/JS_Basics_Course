0f71130837075cbe84a1b5bedca77970a14b8149
Bobb-93 <borisiakimov@gmail.com>
2024-12-14 19:28:01 +0200

    ADDED: lab10 -> 14.TryCatchExample.js

---
+++ labs/lab10/14.TryCatchExample.js
@@ -0,0 +1,12 @@
+try{
+    console.log(x); // JS will throw an Error here, as x is not defined
+    console.log('Continue try block?');
+}catch(err){
+    console.log('There was an error');
+}
+
+console.log('But program continues to run');
+
+// OUTPUT
+// There was an error
+// But program continues to run
