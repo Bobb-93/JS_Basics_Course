3528088353c05c8786fc72294ab4506c37542842
Bobb-93 <borisiakimov@gmail.com>
2024-12-14 14:48:34 +0200

    ADDED: lab10 -> 10.changeColorWithSetInterval.html, .css and .js

---
+++ labs/lab10/10.changeColorWithSetInterval.js
@@ -0,0 +1,18 @@
+var colors = [
+	"red",
+	"green",
+	"blue"
+];
+var i = 0;
+var divNode = document.querySelector(".box");
+
+function changeBGColor() {
+	divNode.style.backgroundColor = colors[i++ % colors.length];
+    console.log(i);
+    //console.log(`i++ % colors.length: ${i++ % colors.length}`);
+    
+    
+}
+
+setInterval(changeBGColor, 1000);
+console.log(`After setTimeout()`);
