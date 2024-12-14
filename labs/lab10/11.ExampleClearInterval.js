56977720b56250907ccc4c2628cab9eabbdb3ad2
Bobb-93 <borisiakimov@gmail.com>
2024-12-14 14:58:19 +0200

    ADDED: 11.ExampleClearInterval.html, .css and .js

---
+++ labs/lab10/11.ExampleClearInterval.js
@@ -0,0 +1,15 @@
+var counterNode = document.querySelector(".counter");
+var stopCounterNode = document.querySelector(".stopCounter");
+var count = 1;
+
+function doCounting() {
+	counterNode.innerHTML = count++;
+}
+
+function stopCounting() {
+	clearInterval(counterID);
+}
+
+stopCounterNode.addEventListener("click", stopCounting);
+
+var counterID = setInterval(doCounting, 1000);
