37724d4e5e91dd4d1061489d63c97b3f705bdf9f
Bobb-93 <borisiakimov@gmail.com>
2024-12-18 18:22:44 +0200

    ADDED: lab10 -> 19.AddClass.html and 20.CatchVariousErrors.js

---
+++ labs/lab10/20.CatchVariousErrors.js
@@ -0,0 +1,22 @@
+try {
+    console.log(x);
+} catch (e){
+    if (e instanceof EvalError){
+        console.log('EvalError')
+    } else if (e instanceof RangeError){
+        console.log('RangeError')
+    } else if (e instanceof ReferenceError){
+        console.log('ReferenceError')
+    } else if (e instanceof SyntaxError){
+        console.log('SyntaxError')
+    } else if (e instanceof TypeError){
+        console.log('TypeError')
+    } else if (e instanceof URIError){
+        console.log('URIError')
+    } else{
+        console.log('Error!')
+    }
+}
+
+// OUTPUT
+// ReferenceError
