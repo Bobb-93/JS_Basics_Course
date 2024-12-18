b5698ecdbaa61f4850737e01cb70094ffe7bc761
Bobb-93 <borisiakimov@gmail.com>
2024-12-18 18:47:55 +0200

    ADDED: lab10 -> 21.CustomExceptions.js

---
+++ labs/lab10/21.CustomExceptions.js
@@ -0,0 +1,23 @@
+function DivideByZero(message) {
+    this.name = "DivideByZero Error";
+    this.message = message || "Can not divide by negative value";
+    this.stack = Error().stack;
+};
+DivideByZero.prototype = new Error;
+
+function divide(x, y){
+    try{
+        if( y === 0 ){
+            throw( new DivideByZero() );
+        }else{
+            return x/y;
+        }
+    }catch(e){
+        console.log( e.name );
+        console.log( `e.stack = ${e.stack}`  );
+        return undefined; // or whatever we want
+    }
+}
+
+console.log( `5/4= ${divide(5, 4)}` );
+console.log( `5/0= ${divide(5, 0)}` );
