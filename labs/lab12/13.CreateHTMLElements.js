97b5b033b6019c345536b2f2776b0d50dba37f7b
Bobb-93 <borisiakimov@gmail.com>
2024-12-21 15:21:48 +0200

    ADDED: lab12 -> 13.CreateHTMLElements.html, .css and .js

---
+++ labs/lab12/13.CreateHTMLElements.js
@@ -0,0 +1,22 @@
+// get the body as element object
+const bodyNode = document.body;
+
+function createElementDemo(){
+	// create header element
+	const headerNode = document.createElement("header");
+	// add class and text content to header element
+	headerNode.className  = 'test'
+	headerNode.textContent = "This header is  created by createElementDemo!!!";
+
+	// attach the header element to the body
+	bodyNode.appendChild(headerNode);	
+}
+
+function setInnerHTMLDemo(){
+	bodyNode.innerHTML = `<header class="test">This header is  created by setInnerHTMLDemo</header>`
+}
+
+setInnerHTMLDemo();
+createElementDemo();
+
+
