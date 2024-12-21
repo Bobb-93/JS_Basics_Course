fdb3a3246b86f7e36f2d9f2632976b84cd000a1c
Bobb-93 <borisiakimov@gmail.com>
2024-12-21 15:14:37 +0200

    ADDED: lab12 -> 12.GetInputValue.html, .css and .js

---
+++ labs/lab12/12.GetInputValue.js
@@ -0,0 +1,13 @@
+// Get DOM nodes:
+const inputNode  = document.querySelector("input[type='text']");
+const outputNode = document.querySelector("#output");
+const btnClick = document.querySelector("button");
+
+// Define showInputValue() function
+function showInputValue( ){
+	outputNode.innerHTML = inputNode.value;
+}
+
+// Add an event listener for the click event on the button
+// When the button is clicked, the showInputValue function will be called
+btnClick.addEventListener("click", showInputValue)
