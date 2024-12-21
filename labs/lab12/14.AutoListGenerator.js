f79bb1c453736491e5d84a06fe3294875a8f3b6c
Bobb-93 <borisiakimov@gmail.com>
2024-12-21 17:22:58 +0200

    ADDED: lab12 -> 14.AutoListGenerator.html, .css and .js

---
+++ labs/lab12/14.AutoListGenerator.js
@@ -0,0 +1,32 @@
+const outputNode = document.getElementById('resultContainer'),
+		listTypeNode = document.getElementById('listType')
+  	listType,
+		listItemsCount;
+const listItemsNode = document.getElementById('listItemsCount');
+
+function createList() {
+	// clean the outputNode content
+	outputNode.innerHTML = '<h1>Резултат</h1>';
+
+	// get list parameters
+	listItemsCount = listItemsNode.value;
+	listType = listTypeNode.value;
+
+	// create the list element
+	const listNode = document.createElement(listType);
+
+	//create list items
+	for (let i = 1; i <= listItemsCount; i++) {
+		// create a li node
+		const listItem = document.createElement('li');
+		
+		// set its content:
+		listItem.innerHTML = `Item ${i}`;
+		
+		// attach the li node to the list container:
+		listNode.appendChild(listItem);
+	}
+
+	// attach the list container to output node:
+	outputNode.appendChild(listNode);
+}
