7a87380221e84534b184c5f652eb43d964b57444
Bobb-93 <borisiakimov@gmail.com>
2024-12-23 12:28:10 +0200

    ADDED: lab12 -> 22.MistakeAddEventListener.html and .js

---
+++ labs/lab12/22.MistakeAddEventListener.js
@@ -0,0 +1,7 @@
+var clickHandler = function() {
+    console.log('click')
+};
+
+// тук ще подадем на addEventListener не нашата функция - clickHandler, 
+// а резултата от нея (в случая - undefined), а самата функция clickHandler() ще бъде извикана моментално:
+document.body.addEventListener( "click", clickHandler() );
