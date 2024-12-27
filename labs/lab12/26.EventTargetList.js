2bd908011571267d626b37672f99cd85fb4d5462
Bobb-93 <borisiakimov@gmail.com>
2024-12-27 18:05:07 +0200

    ADDED: lab12 -> 26.EventTargetList.html, .css and .js

---
+++ labs/lab12/26.EventTargetList.js
@@ -0,0 +1,16 @@
+var dom = {
+	ulNode: document.querySelector(".player ul"),
+	outputNode: document.querySelector(".output")
+};
+
+function onUlClick(e) {
+	console.dir(e.target);
+	//console.dir(e.currentTarget);
+
+	// we attach the event on the UL, but the event.target contains the actual element being clicked!
+	dom.outputNode.innerText = e.target.innerText;
+	//dom.outputNode.innerText = e.currentTarget.innerText;
+
+}
+
+dom.ulNode.addEventListener("click", onUlClick);
