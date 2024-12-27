b9e21362073116b0b245b6d0fc61c351cf1a2741
Bobb-93 <borisiakimov@gmail.com>
2024-12-27 17:35:34 +0200

    ADDED: lab12 -> 25.TargetVsCurrentTarget.html, .css and .js

---
+++ labs/lab12/25.TargetVsCurrentTarget.js
@@ -0,0 +1,16 @@
+let redNode = document.querySelector("#red");
+let greenNode = document.querySelector("#green");
+let outputNode = document.querySelector(".output");
+
+redNode.addEventListener("click", function (e) {
+	outputNode.innerHTML += `<br>Target: ${e.target.id} was clicked<br>`
+	outputNode.innerHTML += `<br>Current Target: ${e.currentTarget.id} <br>`
+});
+
+greenNode.addEventListener("click", function(){
+	outputNode.innerHTML += "Green was clicked<br>"
+});
+
+document.body.addEventListener("click", function(){
+	outputNode.innerHTML += "Body was clicked<br>"
+});
