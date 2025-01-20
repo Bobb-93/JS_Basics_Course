07e503a7e76174b45f39be92dea9e679fb702b94
Bobb-93 <borisiakimov@gmail.com>
2025-01-20 20:23:22 +0200

    add lab16 -> 11.methodDefinitions.js

---
+++ labs/lab16/11.methodDefinitions.js
@@ -0,0 +1,11 @@
+let p1 = {
+    name: 'Pesho',
+    
+    // greet: function greet(){
+    // greet: function(){
+    greet(){
+        console.log(`Hi, I'm ${this.name}`);
+    },
+}
+
+p1.greet(); // Hi, I'm Pesho
