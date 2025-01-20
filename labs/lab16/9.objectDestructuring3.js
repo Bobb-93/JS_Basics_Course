c637dc8dcd5688e382bc7a112006f4af149bf4b3
Bobb-93 <borisiakimov@gmail.com>
2025-01-20 20:13:15 +0200

    add lab16 -> 9.objectDestructuring3.js

---
+++ labs/lab16/9.objectDestructuring3.js
@@ -0,0 +1,26 @@
+const userData = {
+    id: 1,
+    name:'Ada',
+    age: 23,
+};
+
+function greetPlain( name, age ){
+    //console.log(userData);
+    
+    console.log(`Hello ${name}. You are ${age} years old!`);
+}
+
+greetPlain(userData.name,userData.age);
+
+// note the curly braces in params declaration
+function greetObject( {name,age} ){
+    //console.log(userData);
+    
+    console.log(`Hello ${name}. You are ${age} years old!`);
+}
+
+greetObject(userData);
+
+/*
+    If we have the same name function, we get Hello undefined. You are undefined years old! for the first call.
+*/
