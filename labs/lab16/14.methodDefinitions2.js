a80e3bce2999842b69519bf76c0dbff9ac6c4473
Bobb-93 <borisiakimov@gmail.com>
2025-01-20 20:46:55 +0200

    add lab16 -> 14.methodDefinitions2.js

---
+++ labs/lab16/14.methodDefinitions2.js
@@ -0,0 +1,13 @@
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    greet(){
+        console.log(`I'm ${this.name}, ${this.age} years old.`);
+    }
+}
+
+let p1 = new Person('Pesho', 23);
+console.dir(p1);
