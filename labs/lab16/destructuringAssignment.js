ecd2505e6bee17126c886d773abac86e6cb1a61b
Bobb-93 <borisiakimov@gmail.com>
2025-01-23 15:57:28 +0200

    change destructuringAssignment.js

--- labs/lab16/destructuringAssignment.js
+++ labs/lab16/destructuringAssignment.js
@@ -6,22 +6,26 @@
 // console.log(userName);
 // console.log(userAge);
 
-
+// console.log('--------');
 // let [a,b] = [1,2,3];
 // console.log(a,b);
 
-// let [a,b] = [1];
-// console.log(a,b);
+// let [a1,b1] = [1];
+// console.log(a1,b1);
 
+// console.log('swap');
 
-// TASK: swap vaiable values
+// // TASK: swap vaiable values
 // let x = 1;
 // let y = 2;
 
-// before ES6
+// // before ES6
 // let z = y;
 // y=x;
 // x=z;
+
+// console.log(x,y);
+
 
 // // after ES6
 // [x,y] = [y,x];
@@ -30,6 +34,7 @@
 // console.log(x); // 2
 // console.log(y); // 1
 
+console.log('**********');
 
 // let data = {
 //     userName:'Ada',
@@ -46,13 +51,13 @@
 // console.log(userAge);
 
 // Remove object properties
-// let obj = {'a':1,'b':2,'c':3}
+let obj = {'a':1,'b':2,'c':3}
 
-// // 'c' will go into c, and the rest of obj into obj2
-// let {c,b,...obj2} = obj
+// 'c' will go into c, and the rest of obj into obj2
+let {c,b,...obj2} = obj
 
-// console.log(c);
-// console.log(obj2);
+console.log(c);
+console.log(obj2);
 
 const userData = {
     id: 1,
@@ -69,5 +74,5 @@ function greet( {name, age} ){
     console.log(`Hello ${name}. You are ${age} years old!`);
 }
 
-greet(userData)
+greet(userData);
 
