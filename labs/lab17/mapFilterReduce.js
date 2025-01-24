921db65e0803030a60e437eac29104749a6126a8
Bobb-93 <borisiakimov@gmail.com>
2025-01-24 19:06:58 +0200

    add lab17 -> mapFilterReduce.js map Task1

--- labs/lab17/mapFilterReduce.js
+++ labs/lab17/mapFilterReduce.js
@@ -79,25 +79,24 @@ console.log( output3 );// [2,4]
 
 
 // // TASK: filter only cities which population is greater than 340_000
-// const cities = [
-//     {name: 'Sofia', population: 1_236_000},
-//     {name: 'Plovdiv', population: 343_424 },
-//     {name: 'Burgas', population: 202_766},
-//     {name: 'Varna', population: 335_177},
-// ];
+const cities2 = [
+    {name: 'Sofia', population: 1_236_000},
+    {name: 'Plovdiv', population: 343_424 },
+    {name: 'Burgas', population: 202_766},
+    {name: 'Varna', population: 335_177},
+];
 
-// // YOUR CODE HERE:
-// const populatedCities = cities.filter( city=>city.population>340_000 );
+// YOUR CODE HERE:
+const populatedCities = cities2.filter( city=>city.population>340_000 );
 
-// // TEST:
-// console.log(populatedCities);
+// TEST:
+console.log(populatedCities);
 
-// EXPECTED OUTPUT:
-// [
-// 	{ name: 'Sofia', population: 1236000 },
-// 	{ name: 'Plovdiv', population: 343424 }
-// ]
-
+// // EXPECTED OUTPUT:
+// // [
+// // 	{ name: 'Sofia', population: 1236000 },
+// // 	{ name: 'Plovdiv', population: 343424 }
+// // ]
 
 
 
