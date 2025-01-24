31fe17937ce0d08dff3ec5c5022f3847f646b2e0
Bobb-93 <borisiakimov@gmail.com>
2025-01-24 11:41:48 +0200

    add lab17 -> 3.mapArrowSyntax.js

---
+++ labs/lab17/3.mapArrowSyntax.js
@@ -0,0 +1,9 @@
+let input = ['a', 'b', 'c', 'd'];
+
+let output = input.map( e=>e.toUpperCase() )
+
+console.log(`input: ${input}`);
+console.log(`output: ${output}`);
+
+//input: a,b,c,d
+//output: A,B,C,D
