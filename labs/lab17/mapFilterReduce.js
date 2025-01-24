1e40863762856097fd8c74c79190ed7cead45993
Bobb-93 <borisiakimov@gmail.com>
2025-01-24 11:57:06 +0200

    add lab17 -> mapFilterReduce.js (work on map Task 2)

--- labs/lab17/mapFilterReduce.js
+++ labs/lab17/mapFilterReduce.js
@@ -44,28 +44,28 @@ console.log(cityNames);
 // TASK: from 'cities' array generate a new array 'bgCityNames' which will contain only the names
 // of the cities, but mapped to their Bulgarian equivalent
 
-// let dict = {
-//     'Sofia' : 'София',
-//     'Plovdiv' : 'Пловдив',
-//     'Burgas' : 'Бургас',
-//     'Varna' : 'Варна'
-// };
+let dict = {
+    'Sofia' : 'София',
+    'Plovdiv' : 'Пловдив',
+    'Burgas' : 'Бургас',
+    'Varna' : 'Варна'
+};
 
 
-// let cities = [
-//     {name: 'Sofia', population: 1_236_000},
-//     {name: 'Plovdiv', population: 343_424 },
-//     {name: 'Burgas', population: 202_766},
-//     {name: 'Varna', population: 335_177},
-// ];
+let cities1 = [
+    {name: 'Sofia', population: 1_236_000},
+    {name: 'Plovdiv', population: 343_424 },
+    {name: 'Burgas', population: 202_766},
+    {name: 'Varna', population: 335_177},
+];
 
-// // YOUR CODE HERE:
-// const bgCityNames = cities
-//     .map( city=>city.name )
-//     .map(cityName=>dict[cityName]);
+// YOUR CODE HERE:
+const bgCityNames = cities1
+    .map( city=>city.name )
+    .map(cityName=>dict[cityName]);
 
-// // TEST:
-// console.log(bgCityNames);
+// TEST:
+console.log(bgCityNames);
 
 // EXPECTED OUTPUT:
 // [ 'София', 'Пловдив', 'Бургас', 'Варна' ]
