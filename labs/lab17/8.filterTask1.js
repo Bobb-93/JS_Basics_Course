13c8c22956034e93345035573c29b4bf36723038
Bobb-93 <borisiakimov@gmail.com>
2025-01-24 19:07:49 +0200

    add lab17 -> 8.filterTask1.js

---
+++ labs/lab17/8.filterTask1.js
@@ -0,0 +1,18 @@
+const cities = [
+    {name: 'Sofia', population: 1_236_000},
+    {name: 'Plovdiv', population: 343_424 },
+    {name: 'Burgas', population: 202_766},
+    {name: 'Varna', population: 335_177},
+];
+
+// YOUR CODE HERE:
+const populatedCities = cities.filter(city=>city.population>340_000)
+
+// TEST:
+console.log(populatedCities);
+
+// OUTPUT:
+// [
+// 	{ name: 'Sofia', population: 1236000 },
+// 	{ name: 'Plovdiv', population: 343424 }
+// ]
