3fa6ebcea537ecacbcfe65a1b367e4e090391675
Bobb-93 <borisiakimov@gmail.com>
2025-01-24 18:59:38 +0200

    add lab17 -> 6.mapTask2.js

---
+++ labs/lab17/6.mapTask2.js
@@ -0,0 +1,21 @@
+let dict = {
+    'Sofia' : 'София',
+    'Plovdiv' : 'Пловдив',
+    'Burgas' : 'Бургас',
+    'Varna' : 'Варна'
+};
+let cities1 = [
+            {name: 'Sofia', population: 1_236_000},
+            {name: 'Plovdiv', population: 343_424 },
+            {name: 'Burgas', population: 202_766},
+            {name: 'Varna', population: 335_177},
+];
+
+// YOUR CODE HERE:
+const bgCityNames = cities1.map(city=>dict[city.name])
+
+// TEST:
+console.log(bgCityNames);
+
+// OUTPUT:
+// [ 'София', 'Пловдив', 'Бургас', 'Варна' ]
