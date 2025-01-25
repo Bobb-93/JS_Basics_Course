ac1446ee9d2841b084db075068b24266e5bdd064
Bobb-93 <borisiakimov@gmail.com>
2025-01-25 11:12:06 +0200

    add lab17 -> 18.sortTask1.js

---
+++ labs/lab17/18.sortTask1.js
@@ -0,0 +1,20 @@
+const cities = [
+    {name: 'Sofia', population: 1_236_000},
+    {name: 'Plovdiv', population: 343_424},
+    {name: 'Burgas', population: 202_766},
+    {name: 'Varna', population: 335_177},
+];
+
+// YOUR CODE HERE:
+const sortedCities = cities.sort((a, b) => b.population - a.population);
+
+// TEST:
+console.log(sortedCities);
+
+// OUTPUT:
+// [
+//   { name: 'Sofia', population: 1236000 },
+//   { name: 'Plovdiv', population: 343424 },
+//   { name: 'Varna', population: 335177 },
+//   { name: 'Burgas', population: 202766 }
+// ]
