12c39d4268e495cf9147ebae5a10deba36320a80
Bobb-93 <borisiakimov@gmail.com>
2025-01-29 20:28:09 +0200

    add lab 18 folder -> 9.asyncAwaitExample.js

---
+++ labs/lab18/9.asyncAwaitExample.js
@@ -0,0 +1,17 @@
+let getDataPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Some data');
+        // reject('Error message');
+    }, 1000);
+});
+
+async function renderData() {
+    try {
+        const data = await getDataPromise
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+renderData();
