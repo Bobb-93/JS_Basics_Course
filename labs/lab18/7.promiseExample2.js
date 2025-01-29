4d6fbc4519c8de4d8f5e8334340818eb16ae1594
Bobb-93 <borisiakimov@gmail.com>
2025-01-29 15:00:36 +0200

    add lab 18 folder -> 7.promiseExample2.js

---
+++ labs/lab18/7.promiseExample2.js
@@ -0,0 +1,21 @@
+// create Promise object:
+const p = new Promise((resolve, reject)=>{
+    // do promised work asynchronously, like getting data
+    setTimeout(() => {
+        const data = 'Data fetched...';
+        // const data = false;
+
+        if(data){
+            resolve(data)
+        }else{
+            reject('Error')
+        }
+    }, 1000);
+});
+
+// use promise:
+p.then(value => {
+    console.log(value);
+}, reason => {
+    console.error(reason);
+});
