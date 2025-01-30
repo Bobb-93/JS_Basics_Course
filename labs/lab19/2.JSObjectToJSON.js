a3be252c1452a493ee1ab8017f4898e6d8564cce
Bobb-93 <borisiakimov@gmail.com>
2025-01-30 15:26:58 +0200

    lab 19 folder -> add 2.JSObjectToJSON.js

---
+++ labs/lab19/2.JSObjectToJSON.js
@@ -0,0 +1,13 @@
+// this is a JS object
+const data = {
+	"title": "Learn HTML",
+	"completed": true,
+	"id": 1
+};
+console.log(typeof data);
+console.log(data);
+
+// let's convert it into JSON string:
+const dataJSON = JSON.stringify(data)
+console.log(typeof dataJSON);
+console.log(dataJSON);
