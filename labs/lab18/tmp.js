4e37ab7a7f909de498d4a3128acf8e6d4f226a2e
Bobb-93 <borisiakimov@gmail.com>
2025-01-30 11:59:46 +0200

    lab 18 folder -> add tmp.js

---
+++ labs/lab18/tmp.js
@@ -0,0 +1,32 @@
+const handleError = err=>{
+    console.error(`Error: ${err}`)
+};
+
+const prepareDinner = ()=>{
+    console.log(`prepareDinner will take 2000ms`);
+    setTimeout(() => {
+        // do work on prepare dinner
+        const dinner = 'pizza';
+        // const dinner = false;
+
+        if(dinner){
+            console.log(`${dinner} is prepared!`);
+            eatDinner(dinner);
+        }else{
+            handleError(`Can not buy dinner!`);
+        }
+    }, 2000);
+};
+
+//moms spaghetti 
+const eatDinner = (dinner)=> console.log(`Eating ${dinner}`);
+
+
+const dinner=prepareDinner() ; // undef
+
+
+console.log(`Do something else`);
+
+// prepareDinner will take 2000ms
+// Eating undefined
+// Pizza is prepared
