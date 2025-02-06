749dfbd2e204810e72e3111ed7c400f6bf5206db
Bobb-93 <borisiakimov@gmail.com>
2025-02-06 11:46:25 +0200

    lab20 -> add 8.PATCHRequestExampleAsyncAwait.js

---
+++ labs/lab20/8.PATCHRequestExampleAsyncAwait.js
@@ -0,0 +1,19 @@
+async function updatePostPartial() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'PATCH',
+            body: JSON.stringify({
+                title: 'Partially Updated Post',
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+updatePostPartial();
