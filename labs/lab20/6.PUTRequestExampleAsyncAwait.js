1a593e211839fcd64636c5118baa821278cacb37
Bobb-93 <borisiakimov@gmail.com>
2025-02-06 11:42:26 +0200

    lab20 -> add 6.PUTRequestExampleAsyncAwait.js

---
+++ labs/lab20/6.PUTRequestExampleAsyncAwait.js
@@ -0,0 +1,21 @@
+async function updatePost() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'PUT',
+            body: JSON.stringify({
+                title: 'Updated Post',
+                body: 'Updated content',
+                userId: 1,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+updatePost();
