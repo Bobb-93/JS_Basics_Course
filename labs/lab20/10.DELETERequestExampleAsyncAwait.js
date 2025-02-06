3856ea4235fe4f06e326eec1f4562c9ba2bf58a9
Bobb-93 <borisiakimov@gmail.com>
2025-02-06 11:48:41 +0200

    lab20 -> add 10.DELETERequestExampleAsyncAwait.js

---
+++ labs/lab20/10.DELETERequestExampleAsyncAwait.js
@@ -0,0 +1,12 @@
+async function deletePost() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'DELETE',
+        });
+        console.log('Deleted with status:', response.status);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+deletePost();
