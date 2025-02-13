c361ff667f14f72041e0175fca0cb2b421c35fbe
Bobb-93 <borisiakimov@gmail.com>
2025-02-13 11:43:49 +0200

    lab 22 -> ES6toES5.js -> converted to ES5

--- labs/lab22/ES6toES5.js
+++ labs/lab22/ES6toES5.js
@@ -1,30 +1,40 @@
-class Animal {
-    // "name":string
-
-    constructor(name) {
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Animal = /** @class */ (function () {
+    function Animal(name) {
         this.name = name;
     }
-
-    eat(){
-        console.log(`${this.name} is eating`);
+    Animal.prototype.eat = function () {
+        console.log("".concat(this.name, " is eating"));
+    };
+    Animal.prototype.speak = function () {
+        console.log("".concat(this.name, " makes an animal noise"));
+    };
+    return Animal;
+}());
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog(name) {
+        return _super.call(this, name) || this;
     }
-
-    speak() {
-        console.log(`${this.name} makes an animal noise`);
-    }
-}
-
-class Dog extends Animal {
-    constructor(name) {
-        super(name);
-    }
-
-    speak() {
-        console.log(`${this.name} barks`);
-    }
-}
-
+    Dog.prototype.speak = function () {
+        console.log("".concat(this.name, " barks"));
+    };
+    return Dog;
+}(Animal));
 var pluto = new Dog('Pluto');
-
 pluto.speak();
 pluto.eat();
